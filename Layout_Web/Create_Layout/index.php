c5a7900d3fff73755b68ae895b7edb932783da53
manhquynh3107 <70877747+manhquynh3107@users.noreply.github.com>
2020-11-05 11:54:12 +0700

    Add files via upload

---
+++ Layout_Web/Create_Layout/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template Of Layout</title>
+</head>
+<body>
+<div id="main">
+    <div id="head"></div>
+    <div id="headlink"></div>
+    <div id="left"></div>
+    <div id="content"></div>
+    <div id="right"></div>
+    <div id="footer"></div>
+
+</div>
+</body>
+</html>
+<?php
